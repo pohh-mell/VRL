f1069cce76d40475815bad4d87357b1c382fb40c
Hilari <hilarimuts@gmail.com>
2015-02-24 14:54:19 +0200

    katse 4 v

--- ostukorv2.php
+++ ostukorv2.php
@@ -1,46 +1,15 @@
 <?
-$otsitav = $_POST['otsitav'];
-$kordi = $_POST['kordi'];
-if ($otsitav == 0) {
-	$otsitav = rand(1,30);
-	$kordi = 0;
-	//  muutuja $kordi loendab pakkumisi
+$arv = rand(1,10);
+$arv2 = rand(1,10);
+$arv3 = $arv-$arv2;
+$arv4 = $arv2-$arv;
+if ($arv > $arv2) {
+	echo "Esimene arv $arv on $arv3 võrra suurem kui arv $arv2.";
 }
-// $otsitav saab juhusliku väärtuse vahemikus 1-30.
-$pakkumine = $_POST['arv'];
-if ($pakkumine) {
-	//kontrollime kas kasutaja tegi pakkumise
-	$kordi = $kordi + 1;
-	// suurendame muutuja $kordi väärtus 1 võrra
+else if ($arv == $arv2) {
+	echo "Arv $arv on võrdne arvuga $arv2.";
 }
-if ($pakkumine == $otsitav) {
-// kui kasutaja on ära arvanud õige arvu, siis nullime muutuja $otsitav
-	$otsitav = 0;
-}
+else {
+	echo "Esimene arv $arv on $arv4 võrra väiksem kui arv $arv2.";
+}	
 ?>
-<html>
-<head>
-	<title>Arvamismäng</title>
-</head>
-<body>
-	<form method="post">
-	Tee oma pakkumine;
-		<input type="text" name="arv" size="5" />
-		<input type="hidden" name="otsitav" value="<?= $otsitav ?>" />
-		<input type="hidden" name="kordi" value="<?= $kordi ?>" />
-		<input type="submit" value="OK" />
-	</form>
-<?
-if ($pakkumine && $pakkumine > $_POST['otsitav']) {
-	echo "Sinu pakutud arv $pakkumine on liiga suur";
-}
-if ($pakkumine && $pakkumine < $_POST['otsitav']) {
-	// kas muutujal $pakkumine on väärtus JA kas $pakkumine on väiksem kui $otsitav
-	echo "Sinu pakutud arv $pakkumine on liiga väike";
-}
-if ($pakkumine && $pakkumine == $_POST['otsitav']) {
-	echo "Sinu pakutud arv $pakkumine on õige! Arvamiseks kulus $kordi korda. Arva järgmist arvu.";
-}
-?>
-</body>
-</html>
