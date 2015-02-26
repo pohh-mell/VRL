22c841eed70063b88fb68ec78e444ad8785682de
Hilari <hilarimuts@gmail.com>
2015-02-26 17:27:03 +0200

    yoyoyoyoyo

--- hellowrld.php
+++ hellowrld.php
@@ -1,48 +1,51 @@
 <html>
 	<head>
-  		<title>E-hääletus</title>
-  		<link rel="icon" href="http://president.ee/images/stories/president_staatilised/lipp.jpg" type="image/x-icon" />
-  		<meta charset="utf-8">
-  		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-		
+		<title>E-hääletus</title>
+		<link rel="icon" href="http://president.ee/images/stories/president_staatilised/lipp.jpg" type="image/x-icon" />
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
 	</head>
-	<body>
-		<div id="x1">
-			<div id="x2">
-	             <div id="logo">
-	             	<a id="ll" target="_blank">
-		        	<img src="http://president.ee/images/stories/president_staatilised/lipp.jpg" alt="" title="Lipp"/>
-		        	</a>
-	             
-	                <h1><a href="http://www.tdl.ee/~hilari/VRL.php">
-	                    <img src="" alt="" title="">
-	                    <span class="slogan">E-hääletus</span>
-	                    </a>
-	                </h1>
-	             </div>
-	        <div id="header-right">
-		        <div id="site-search">
-		            <form name="search" id="search" method="get" action="/otsing/">
-		              	<div class="input">
-		                    <input class="input-box" type="text" name="search" value=""/>
-		                    <input type="submit" style="display:none;"/>
-		                </div>
-					</form>
-		        </div>
-		    </div>
-		    <div id="x3">
-		        <ul id="lang">
-		        	<li>
-		        		<a href="">ENG</a></li><li> | 
-		                <a href="">FR</a></li><li> | 
-		                <a href="">ESP</a>
-		            </li>  
-		        </ul>
-	        </div>
-         </div>
-         
+	<body  style="background-color:lavender;">
+		<div class="põhi">
+			<div class="page-header">
+				<div class="col-xs-12 col-md-12" style="background-color:#C3D1E0;">
+
+					<div class="col-xs-9 col-md-9">  
+						<h1>
+							<a href="http://www.tdl.ee/~hilari/VRL.php">
+								<h1></br>E-hääletus</h1>
+							</a>
+						</h1>
+					</div>
+					<div class ="col-xs-2 col-md-3 ">
+						<div class ="col-xs-12 col-md-12"
+						<a href =""></br><p class="text-center">SISENE</p></a>		  
+					</div>
+					<div class ="col-xs-12 col-md-12">
+
+						<p class="text-center"><a href ="">ENG</a>		              
+							<a href ="">EE</a></p>
+						</div>
+						<div class ="col-xs-12 col-md-12">
+							<a href =""><p class="text-center">olx</p></a>		
+						</div>
+					</div> 
+				</div>
+
+			<div class="page-header">
+
+		</div>
+
 	</body>
 </html>
