e68a995f3885bab2a2ea41a45287ba0324c6cfcb
Hilari <hilarimuts@gmail.com>
2015-02-26 14:52:44 +0200

    aa

--- hellowrld.php
+++ hellowrld.php
@@ -1,3 +1,48 @@
-<?
-echo "Hello world!";
-?>
+<html>
+	<head>
+  		<title>E-hääletus</title>
+  		<link rel="icon" href="http://president.ee/images/stories/president_staatilised/lipp.jpg" type="image/x-icon" />
+  		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		
+	</head>
+	<body>
+		<div id="x1">
+			<div id="x2">
+	             <div id="logo">
+	             	<a id="ll" target="_blank">
+		        	<img src="http://president.ee/images/stories/president_staatilised/lipp.jpg" alt="" title="Lipp"/>
+		        	</a>
+	             
+	                <h1><a href="http://www.tdl.ee/~hilari/VRL.php">
+	                    <img src="" alt="" title="">
+	                    <span class="slogan">E-hääletus</span>
+	                    </a>
+	                </h1>
+	             </div>
+	        <div id="header-right">
+		        <div id="site-search">
+		            <form name="search" id="search" method="get" action="/otsing/">
+		              	<div class="input">
+		                    <input class="input-box" type="text" name="search" value=""/>
+		                    <input type="submit" style="display:none;"/>
+		                </div>
+					</form>
+		        </div>
+		    </div>
+		    <div id="x3">
+		        <ul id="lang">
+		        	<li>
+		        		<a href="">ENG</a></li><li> | 
+		                <a href="">FR</a></li><li> | 
+		                <a href="">ESP</a>
+		            </li>  
+		        </ul>
+	        </div>
+         </div>
+         
+	</body>
+</html>
