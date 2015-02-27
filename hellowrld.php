d597b99b3bfe5b18d6eb59c62d1f8884751bb39a
Hilari <hilarimuts@gmail.com>
2015-02-27 18:02:28 +0200

    kk

--- hellowrld.php
+++ hellowrld.php
@@ -19,7 +19,7 @@
 	<body  style="background-color:lavender;">
 		<div class="põhi">
 			<div class="page-header">
-				<div class="col-xs-12 col-md-12" style="background-color:#C3D1E0;">
+				<div class="col-xs-12 col-md-12" style="background-color:white;">
 
 					<div class="col-xs-9 col-md-9">  
 						<h1>
@@ -29,22 +29,64 @@
 						</h1>
 					</div>
 					<div class ="col-xs-2 col-md-3 ">
-						<div class ="col-xs-12 col-md-12"
-						<a href =""></br><p class="text-center">SISENE</p></a>		  
+						<div class ="col-xs-12 col-md-12">
+						
+						<p class="text-center">
+							</br>SISENE: 
+							<a href="">
+    							<img src="https://www.eesti.ee/riigiportaal/gfx/logo_id.gif"/>
+  							</a>
+						</p>		  
 					</div>
+					
+					<div class ="col-xs-12 col-md-12">
+						<a href =""><p class="text-center"></br></p></a>		
+					</div>
+						
+					
 					<div class ="col-xs-12 col-md-12">
 
-						<p class="text-center"><a href ="">ENG</a>		              
-							<a href ="">EE</a></p>
-						</div>
-						<div class ="col-xs-12 col-md-12">
-							<a href =""><p class="text-center">olx</p></a>		
+						<p class="text-center">
+							<a href="">
+    							<img src="http://flags.redpixart.com/img/1570/flag_256.jpg" id="ENG" style="width:13%"/>
+  							</a>
+							<a href="">
+    							<img src="http://flags.redpixart.com/img/1445/flag_256.jpg" id="EE" style="width:13%"/>
+  							</a>
+						</p>
 						</div>
 					</div> 
 				</div>
-
+			
 			<div class="page-header">
-
+				<div class="page-header">
+				<div class="col-xs-12 col-md-12" style="text-align:center" >
+					<div class="col-xs-12 col-md-12" >
+					</br></br></br></br>
+					</div>
+					<a href="">
+					<div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
+						</br><h3>KANDIDAADID</h3></br></br>
+					</div>
+					</a>
+					<a href=""><div class="col-xs-4 col-md-4 col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
+						<h3>
+    							<img src="http://tinyurl.com/ptxtreq" style="width:20%" />
+  						
+						STATISTIKA</h3>
+					</div>
+					</a>
+					<div class="col-xs-12 col-md-12" >
+					</br></br></br></br>
+					</div>
+					<a href="">
+					<div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
+						</br><h3>TULEMUSED</h3></br></br>
+					</div>
+					</a>
+				</div>
+			</div>
+			</div>
 		</div>
 
 	</body>
