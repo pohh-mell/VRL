87caec8ba9c11781433d3a4de49b02de93a0b295
Hilari <hilarimuts@gmail.com>
2015-03-02 12:57:05 +0200

    k

--- hellowrld.php
+++ hellowrld.php
@@ -1,93 +1,78 @@
 <html>
-	<head>
-		<title>E-hääletus</title>
-		<link rel="icon" href="http://president.ee/images/stories/president_staatilised/lipp.jpg" type="image/x-icon" />
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<title>E-hääletus</title>
+	<link rel="icon" href="http://president.ee/images/stories/president_staatilised/lipp.jpg" type="image/x-icon" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-		<!-- Latest compiled JavaScript -->
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-
-	</head>
-	<body  style="background-color:lavender;">
-		<div class="põhi">
-			<div class="page-header">
-				<div class="col-xs-12 col-md-12" style="background-color:white;">
-
-					<div class="col-xs-9 col-md-9">  
-						<h1>
-							<a href="">
-								<h1></br>E-hääletus</h1>
-							</a>
-						</h1>
-					</div>
-					<div class ="col-xs-2 col-md-3 ">
-						<div class ="col-xs-12 col-md-12">
-						
-						<p class="text-center">
-							</br>SISENE: 
-							<a href="">
-    							<img src="https://www.eesti.ee/riigiportaal/gfx/logo_id.gif"/>
-  							</a>
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color:#DEDEFF;">
+	<div class="põhi">
+		<div class="page-header">
+			<div class="col-xs-12 col-md-12" style="background-color:white">
+				<div class="col-xs-9 col-md-9">  
+					<h1><a href=""></br>E-hääletus</a></h1>
+				</div>
+				<div class ="col-xs-2 col-md-3 " style="text-align:center">
+					<div class ="col-xs-12 col-md-12">
+						<p></br>SISENE: 
+							<a href=""><img src="https://www.eesti.ee/riigiportaal/gfx/logo_id.gif"/></a>
 						</p>		  
 					</div>
-					
 					<div class ="col-xs-12 col-md-12">
-						<a href =""><p class="text-center"></br></p></a>		
+						<p><a href =""></br></a></p>
 					</div>
-						
-					
 					<div class ="col-xs-12 col-md-12">
-
-						<p class="text-center">
-							<a href="">
-    							<img src="http://flags.redpixart.com/img/1570/flag_256.jpg" id="ENG" style="width:13%"/>
-  							</a>
-							<a href="">
-    							<img src="http://flags.redpixart.com/img/1445/flag_256.jpg" id="EE" style="width:13%"/>
-  							</a>
+						<p>
+							<a href=""><img src="http://flags.redpixart.com/img/1570/flag_256.jpg" id="ENG" style="width:13%"/></a>
+							<a href=""><img src="http://flags.redpixart.com/img/1445/flag_256.jpg" id="EE" style="width:13%"/></a>
 						</p>
-						</div>
-					</div> 
+					</div>
 				</div>
-			
-			<div class="page-header">
-				<div class="page-header">
+			 </div>
+		</div>
+		<!-- style="background-image:url(http://i58.tinypic.com/mrzay9.jpg);background-size: 100%" -->
+		<div class="middle">
+			<div class="col-xs-12 col-md-12" >
 				<div class="col-xs-12 col-md-12" style="text-align:center" >
 					<div class="col-xs-12 col-md-12" >
-					</br></br></br></br>
+						</br></br></br></br>
 					</div>
-					<a href="">
-					<div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
-						</br><h3>KANDIDAADID</h3></br></br>
-					</div>
-					</a>
+					<a href=""><div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
+							<h3></br>KANDIDAADID</br></br></h3>
+					</div></a>
 					<a href=""><div class="col-xs-4 col-md-4 col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
-						<h3>
-    							<img src="http://tinyurl.com/ptxtreq" style="width:20%" />
-  						
-						STATISTIKA</h3>
-					</div>
-					</a>
+						<h3><img src="http://tinyurl.com/ptxtreq" style="width:20%" /> STATISTIKA</h3>
+					</div></a>
 					<div class="col-xs-12 col-md-12" >
-					</br></br></br></br>
+						</br></br></br></br>
 					</div>
-					<a href="">
-					<div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
-						</br><h3>TULEMUSED</h3></br></br>
+					<a href=""><div class="col-xs-4 col-md-4  col-xs-offset-1 col-md-offset-1" style="background-color:#8585FF;">
+						<h3></br>TULEMUSED</br></br></h3>
+					</div></a>
+					<div class="col-xs-12 col-md-12" >
+						</br></br></br></br>
 					</div>
-					</a>
+					<div class="col-xs-12 col-md-12" >
+						</br></br></br></br>
+					</div>
+					<div class="col-xs-12 col-md-12" >
+						</br></br></br></br>
+					</div>
+					<div class="col-xs-12 col-md-12" >
+						</br></br></br></br>
+					</div>
 				</div>
 			</div>
-			</div>
 		</div>
-
-	</body>
+	</div>
+</body>
 </html>
