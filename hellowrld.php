39625a8cfc6414a8360e5ea2b8b89b8104d2e5e6
Hilari <hilarimuts@gmail.com>
2015-03-03 11:21:10 +0200

    stylesheet

--- hellowrld.php
+++ hellowrld.php
@@ -5,8 +5,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="kujundus.css">
 
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -15,29 +18,31 @@
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="põhi">
-		<div class="page-header">
-			<div class="col-xs-12 col-md-12" style="background-color:#DEDEFF">
-				<div class="col-xs-9 col-md-9">  
-					<h1><a href=""></br>E-hääletus</a></h1>
+	<div class="container">
+		<div class="page-header" style="margin: 0px">
+			
+				<div class="col-xs-12 col-md-12" style="background-color:#DEDEFF">
+					<div class="col-xs-9 col-md-9">  
+						<h1><a href=""></br>E-hääletus</a></h1>
+					</div>
+					<div class ="col-xs-2 col-md-3 " style="text-align:center">
+						<div class ="col-xs-12 col-md-12">
+							<p></br>SISENE: 
+								<a href=""><img src="picid/logo_id.gif"/></a>
+							</p>		  
+						</div>
+						<div class ="col-xs-12 col-md-12">
+							<p><a href =""></br></a></p>
+						</div>
+						<div class ="col-xs-12 col-md-12">
+							<p>
+								<a href=""><img src="picid/ENG.jpg" id="ENG" style="width:13%"/></a>
+								<a href=""><img src="picid/EE.jpg" id="EE" style="width:13%"/></a>
+							</p>
+						</div>
+					</div>
 				</div>
-				<div class ="col-xs-2 col-md-3 " style="text-align:center">
-					<div class ="col-xs-12 col-md-12">
-						<p></br>SISENE: 
-							<a href=""><img src="picid/logo_id.gif"/></a>
-						</p>		  
-					</div>
-					<div class ="col-xs-12 col-md-12">
-						<p><a href =""></br></a></p>
-					</div>
-					<div class ="col-xs-12 col-md-12">
-						<p>
-							<a href=""><img src="picid/ENG.jpg" id="ENG" style="width:13%"/></a>
-							<a href=""><img src="picid/EE.jpg" id="EE" style="width:13%"/></a>
-						</p>
-					</div>
-				</div>
-			 </div>
+			
 		</div>
 		<div class="middle">
 			<div class="col-xs-12 col-md-12" style="background-image:url(picid/kaart.jpg);background-size: 100%">
