a1890915c25056beb56391ea223e4813d9b85e86
Hilari <hilarimuts@gmail.com>
2015-03-05 18:41:26 +0200

    /header

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<link rel="icon" href="picid/lipp.jpg" type="image/x-icon" />
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -18,10 +17,8 @@
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-<script src="javaskript.js">
-	
+<script src="javaskript.js">	
 </script>
-	
 	<header>
 		<div class="container">
 			<div class="row">
@@ -30,9 +27,8 @@
 				</div>
 				<div class ="col-xs-3 col-md-3 col-lg-3" style="text-align:center">
 					<div class ="row4">
-						<p id="status">
-						</br>SISENE: <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-						</fb:login-button>
+						<p id="status"></br>SISENE: 
+							<fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
 						<!--	<a href=""><img src="http://www.helias.cz/pictures/face.png" style="width:10%"/></a>-->
 						</p>		  
 					</div>
@@ -41,13 +37,14 @@
 					</div>
 					<div class ="row">
 						<p>
-							<a href=""><img src="https://cdn2.iconfinder.com/data/icons/world-flag-icons/128/Flag_of_United_Kingdom.png" id="ENG" style="width:10%"/></a>
-							<a href=""><img src="http://png-3.findicons.com/files/icons/656/rounded_world_flags/128/estonia.png" id="EE" style="width:10%"/></a>
+							<a href=""><img src="picid/ENG.png" id="ENG" style="width:10%"/></a>
+							<a href=""><img src="picid/EE.png" id="EE" style="width:10%"/></a>
 						</p>
 					</div>			
 				</div>
 			</div>
 		</div>
+	</header>
 	<div class="container">
 		<div class="middle">		
 			<div class="row">
