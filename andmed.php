599ac90c778a67a27be7b5634b68b982ec8d7ad6
pohh-mell <meelissulg1@gmail.com>
2015-03-17 11:13:45 +0200

    wefwaef

--- andmed.php
+++ andmed.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["kandidaadid.nimi"]. " - Name: " . $row["erakonnad.nimi"].  "<br>";
     }
 } else {
     echo "0 results";
