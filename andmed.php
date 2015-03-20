42961a0456e6766f5aad6bfee9a7e4c3506532f8
pohh-mell <meelissulg1@gmail.com>
2015-03-20 18:17:05 +0200

    tahaks nüüd tabelit näha

--- andmed.php
+++ andmed.php
@@ -34,22 +34,51 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 
-<?php if (count($meielist) > 0): ?>
-<table>
-  <thead>
-    <tr>
-      <th><?php echo implode('</th><th>', array_keys(current($meielist))); ?></th>
-    </tr>
-  </thead>
-  <tbody>
-<?php foreach ($meielist as $row): array_map('htmlentities', $row); ?>
-    <tr>
-      <td><?php echo implode('</td><td>', $row); ?></td>
-    </tr>
-<?php endforeach; ?>
-  <tbody>
-</table>
-<?php endif; ?>
+function build_table($array){
+
+    // start table
+
+    $html = '<table>';
+
+    // header row
+
+    $html .= '<tr>';
+
+    foreach($array[0] as $key=>$value){
+
+            $html .= '<th>' . $key . '</th>';
+
+        }
+
+    $html .= '</tr>';
+
+    // data rows
+
+    foreach( $array as $key=>$value){
+
+        $html .= '<tr>';
+
+        foreach($value as $key2=>$value2){
+
+            $html .= '<td>' . $value2 . '</td>';
+
+        }
+
+        $html .= '</tr>';
+
+    }
+
+    // finish table and return it
+
+    $html .= '</table>';
+
+    return $html;
+
+}
+
+
+
+echo build_table($meielist);
 
 function getAllItems()
 {
