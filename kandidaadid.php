78e9955cb8fae259692355774929df2604936a32
pohh-mell <meelissulg1@gmail.com>
2015-03-20 20:06:35 +0200

    0 resulti :(

--- kandidaadid.php
+++ kandidaadid.php
@@ -82,11 +82,11 @@
 					$sql = "SELECT kandidaadid.number AS Number,kandidaadid.Nimi AS Nimi, kandidaadid.Piirkond AS Piirkond, erakonnad.Nimi AS Erakond, kandidaadid.haali AS Hääli 
 						FROM kandidaadid LEFT JOIN erakonnad 
 						ON kandidaadid.Erakonna_id=erakonnad.id
-						GROUP BY Number;;	";
+						GROUP BY Number;";
 					$result = $conn->query($sql);
 
 					$meielist= array();
-
+					print_r($meielist);
 					if ($result->num_rows > 0) {
    					 // output data of each row
     					while($row = $result->fetch_assoc()) {
