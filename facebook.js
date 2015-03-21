efec8bcabbd39549fa17a6c302be7dcdddd73b80
pohh-mell <meelissulg1@gmail.com>
2015-03-21 21:07:18 +0200

    sdf

--- facebook.js
+++ facebook.js
@@ -8,7 +8,7 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-       Response.Redirect("kandideerimine.php", true);
+     window.location.replace("kandideerimine.php");
       testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
