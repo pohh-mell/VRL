eaaa253718a713c7771a4600d3d62f2ac246366b
Hilari <hilarimuts@gmail.com>
2015-03-21 19:33:14 +0200

    3

--- fb2.js
+++ fb2.js
@@ -1,3 +1,23 @@
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      console.log('Please log into this app.');
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      console.log('Please log into Facebook.');
+    }
+  } 
+
 window.fbAsyncInit = function() {
   FB.init({
       appId      : '764717310292073', // App ID
@@ -16,35 +36,23 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk')); 
 
-function Login(){
-  FB.login(function(response) {
-    if (response.authResponse) {
-      testAPI(); 
-    } else {
-      console.log('Authorization failed.');
-    }
-  }, {scope: 'public_profile,email'});
-}
+ function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    }), {scope: 'public_profile,email'});
+  }
 
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('aff').innerHTML = 'Tere tulemast, ' + response.name + '!';
-      document.getElementById('out').style.visibility="visible";
+      //document.getElementById('aff').innerHTML = 'Tere tulemast, ' + response.name + '!';
+      //document.getElementById('out').style.visibility="visible";
       document.getElementById('kandideerimine').style.visibility="visible";
     });
   }
 
-function myfun(){
-    window.location.search += '&reload=true';
-  }
-  
-
-  function LogOut() {
-    FB.logout(function(response) {
-      console.log('Successful logout for: ' + response.name);
-      myfun();
-     });
-  }
+function Logout(){
+    FB.logout(function(){document.location.reload();});
+}
 
