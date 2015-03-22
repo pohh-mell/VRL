cd82c9ac3de97d3dedbc927f669ce5691a501a7b
pohh-mell <meelissulg1@gmail.com>
2015-03-22 23:49:43 +0200

    ei saanud hakkama

--- logout.php
+++ logout.php
@@ -1,25 +1,8 @@
 <?php 
 session_start();
 session_unset();
-// $url = $_SESSION['url'];
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-//header("Location: http://e-haaletus.azurewebsites.net/$url");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
-//header("Location: index.php");
-if ( isset( $session ) ) {
-
-  // graph api request for user data
-  $request = new FacebookRequest( $session, 'GET', '/me' );
-  $response = $request->execute();
-  // get response
-  $graphObject = $response->getGraphObject();
-      $url = $_SESSION['url'];
-     
-  header("Location: http://e-haaletus.azurewebsites.net/$url");
- 
-} else {
-  $loginUrl = $helper->getLoginUrl();
- header("Location: ".$loginUrl);
-}
+header("Location: index.php");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
 ?>
