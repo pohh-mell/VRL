ef0e7fa2ee3589ea3734ec0dda57302a5288eea9
pohh-mell <meelissulg1@gmail.com>
2015-03-22 23:44:57 +0200

    väljalogides jääks ka samale lehele

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
 <?php 
 session_start();
 session_unset();
+ $url = $_SESSION['url'];
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
     $_SESSION['EMAIL'] =  NULL;
-header("Location: index.php");        // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+header("Location: http://e-haaletus.azurewebsites.net/$url");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
 ?>
