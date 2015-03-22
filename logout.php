ee3a303ef4eeb9db536a0008d3e8698c602ce70a
pohh-mell <meelissulg1@gmail.com>
2015-03-22 23:54:09 +0200

    idisodcfj

--- logout.php
+++ logout.php
@@ -1,8 +1,10 @@
 <?php 
 session_start();
+$url = $_SESSION['url'];
 session_unset();
+
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
-    $_SESSION['EMAIL'] =  NULL;
-header("Location: index.php");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+//header("Location: index.php");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
+	header("Location: http://e-haaletus.azurewebsites.net/$url");
 ?>
