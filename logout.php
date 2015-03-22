ff58ac00c7c8eba0a13f416d595927364cef075f
pohh-mell <meelissulg1@gmail.com>
2015-03-23 00:54:17 +0200

    j

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 $url = $_SESSION['url'];
 session_unset();
-
+	$_SESSION['EMAIL'] =  NULL;
     $_SESSION['FBID'] = NULL;
     $_SESSION['FULLNAME'] = NULL;
 //header("Location: index.php");      // you can enter home page here ( Eg : header("Location: " ."http://www.krizna.com"); 
