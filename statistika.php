17c174916a936f2cdd6a3c6d3d4670ac42ec1c3e
KarlEST <karl.l22ts@hotmail.com>
2015-04-02 15:01:18 +0300

    auh

--- statistika.php
+++ statistika.php
@@ -5,11 +5,12 @@ include "header.php";
 ?>
 	<div class="container">
 		<div class="middle">
+			<ol class="singleline">
 			<li><select class="Valikud" >
 	   		    <option value="Kogu Eesti">Kogu Eesti</option>
 			    <option value="Tartumaa">Partei</option>
-			   
 			</select></li>
+			</ol>
 		</div>	
 	</div>
 <?php include "footer.php"; ?>
