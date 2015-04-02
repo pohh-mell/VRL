f0ecca7a242aa6751ff69722f7f7df78ccb75225
KarlEST <karl.l22ts@hotmail.com>
2015-04-02 14:56:56 +0300

    katsemeelisega

--- statistika.php
+++ statistika.php
@@ -5,7 +5,11 @@ include "header.php";
 ?>
 	<div class="container">
 		<div class="middle">
-			
+			<li><select class="Valikud" >
+	   		    <option value="Kogu Eesti">Kogu Eesti</option>
+			    <option value="Tartumaa">Partei</option>
+			   
+			</select></li>
 		</div>	
 	</div>
 <?php include "footer.php"; ?>
