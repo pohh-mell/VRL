2a98d90413677e5a30daeeddba4fb5095c8f0165
KarlEST <karl.l22ts@hotmail.com>
2015-04-02 15:17:07 +0300

    mellile

--- statistika.php
+++ statistika.php
@@ -3,14 +3,40 @@ $title = "Statistika";
 $link = "http://e-haaletus.azurewebsites.net/";
 include "header.php";
 ?>
+
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+
+
+
+
 	<div class="container">
 		<div class="middle">
 			<ol class="singleline">
-			<li><select class="Valikud" >
+			<li><select onchange="funktioon();">
 	   		    <option value="Kogu Eesti">Kogu Eesti</option>
 			    <option value="Tartumaa">Partei</option>
 			</select></li>
 			</ol>
+
+			<script type="javaskript.js">
+    			function funktioon() {
+          		        
+   				}
+			</script>
+
+
+			<div id="container2" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto">
+				
+				
+
+
+
+
+
+			</div>
+
+
 		</div>	
 	</div>
 <?php include "footer.php"; ?>
