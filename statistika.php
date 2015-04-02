9790920c869b220938f9b0a560adff038ac55166
KarlEST <karl.l22ts@hotmail.com>
2015-04-02 15:40:47 +0300

    töötab?

--- statistika.php
+++ statistika.php
@@ -10,7 +10,7 @@ include "header.php";
 
 
 	<div class="container">
-		<script src="javaskript.js"></script>
+		
 
 		<div class="middle">
 			<ol class="singleline">
@@ -21,13 +21,62 @@ include "header.php";
 			</select></li>
 			</ol>
 
-			
+		
 
 
 			<div id="container2" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto">
 				
 				
-
+				<script type="text/javascript">
+    			function funktioon () {
+    alert("add");
+    console.log("mine");
+    $('#container2').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2014'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['KArl',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+}
+				</script>
 
 
 
