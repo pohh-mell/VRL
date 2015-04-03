f9e7ac51c53906910119aa9df41f3825423481c5
pohh-mell <meelissulg1@gmail.com>
2015-04-03 23:05:06 +0300

    D oli liiga suur

--- kandidaadimuutmine.js
+++ kandidaadimuutmine.js
@@ -1,6 +1,6 @@
 function func(param){
 	alert("ADADAD");
 	var valitud = param;
-	var tabel = document.getElementByID('t01');
+	var tabel = document.getElementById("t01");
 	console.log(tabel.rows);
 }
