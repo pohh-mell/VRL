ceebd5be40b3c5863cae140e799afa88cf45db05
Hilari <hilarimuts@gmail.com>
2015-04-03 17:23:28 +0300

    :((((((

--- kandideerimine.php
+++ kandideerimine.php
@@ -1,51 +1,51 @@
 <?php
-session_start(); 
+session_start();
 $_SESSION['url'] = $_SERVER['REQUEST_URI'];
 ?>
-<?php
-$title = "Kandideerimine";
-include "header.php";
-?>
-<?php 
-if( $_POST ){
-	require_once("dbconfig.php");
-		
-	$nimi = $_POST['nimi'];
-	$isikukood = $_POST['isikukood'];
-	$erakond = $_POST['erakond'];
-	$piirkond= $_POST['piirkond'];
-	$query = "INSERT INTO kandidaadid(Nimi,Piirkond,Erakonna_id,isikukood) VALUES ('$nimi','$piirkond',$erakond,$isikukood);";
-	mysql_query($query);
-	mysql_close($conn);
-}
-?>
-	
-	<div class="container">
-		<div class="middle">
-			<!-- kandidaadi lisamine -->
-			<form class="ff1" action="<?php $_PHP_SELF ?>" method="post">
-				<div class="block">
-	    			<label>Nimi:</label>
-	    			<input type="text" id="nimi" name="nimi">
-				</div>
-				<div class="block">
-	    			<label>Piirkond:</label>
-	    			<input type="text" id="piirkond" name="piirkond">
-				</div>
-				<div class="block">
-	    			<label>Erakond:</label>
-	    			<input type="text" id="erakond" name="erakond">
-				</div>
-				<div class="block">
-	    			<label>Isikukood:</label>
-	    			<input type="text" id="isikukood" name="isikukood">
-				</div>
-				<button class="nupp" type="submit"  id="submit-button">Lisa end kandidaadiks</button>
-			</form>
-		</div>
-	</div>
-	
+ 
+<?php   if( $_POST ){
+                require_once("dbconfig.php");
+               
+ 
+                $nimi = $_POST['nimi'];
+                $isikukood = $_POST['isikukood'];
+                $erakond = $_POST['erakond'];
+                $piirkond= $_POST['piirkond'];
+                $query = "INSERT INTO kandidaadid(Nimi,Piirkond,Erakonna_id,isikukood) VALUES ('$nimi','$piirkond',$erakond,$isikukood);";
+                mysql_query($query);
+ 
+                mysql_close($conn);
+        }
+ 
+ 
+        ?>
+       
+        <div class="container">
+                <div class="middle">
+                        <!-- kandidaadi lisamine -->
+                        <form class="ff1" action="<?php $_PHP_SELF ?>" method="post">
+                                <div class="block">
+                                <label>Nimi:</label>
+                                <input type="text" id="nimi" name="nimi">
+                                </div>
+                                <div class="block">
+                                <label>Piirkond:</label>
+                                <input type="text" id="piirkond" name="piirkond">
+                                </div>
+                                <div class="block">
+                                <label>Erakond:</label>
+                                <input type="text" id="erakond" name="erakond">
+                                </div>
+                                <div class="block">
+                                <label>Isikukood:</label>
+                                <input type="text" id="isikukood" name="isikukood">
+                                </div>
+                                <button class="nupp" type="submit"  id="submit-button">Lisa end kandidaadiks</button>
+                        </form>
+                </div>
+        </div>
+       
 <?php include "footer.php"; ?>
-<?php else: ?>
- <meta http-equiv="refresh" content="0; url=http://e-haaletus.azurewebsites.net/logisisse.php" />
+ <?php else: ?>
+ <meta http-equiv="refresh" content="0; url=http://e-haaletus.azurewebsites.net/index.php" />
 <?php endif ?>
