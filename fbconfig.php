5152899bf22344db56e26394a6242d3abee45f75
Hilari <hilarimuts@gmail.com>
2015-04-04 02:05:23 +0300

    lol i did ittttt, vist. Meldimine done

--- fbconfig.php
+++ fbconfig.php
@@ -17,7 +17,7 @@ use Facebook\HttpClients\FacebookHttpable;
 // init app with app id and secret
 FacebookSession::setDefaultApplication('764717310292073','f7dc16c733615eb9f61a5fa0e41652b7' );
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('http://e-haaletus.azurewebsites.net/fbconfig.php' );
+$helper = new FacebookRedirectLoginHelper('http://e-haaletus.azurewebsites.net/fbconfig.php' );
 try {
     $session = $helper->getSessionFromRedirect();
 } catch( FacebookRequestException $ex ) {
@@ -26,33 +26,30 @@ try {
     // When validation fails or other local issues
 }
 // see if we have a session
-if ( isset( $session ) ) {
-
+if (isset($session)){
     // graph api request for user data
     $request = new FacebookRequest( $session, 'GET', '/me' );
     $response = $request->execute();
     // get response
     $graphObject = $response->getGraphObject();
-     	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
- 	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
-	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
-        $url = $_SESSION['url'];
-        checkuser($fbid,$fbfullname,$femail); // To update local DB
-	/* ---- Session Variables -----*/
-	    $_SESSION['FBID'] = $fbid;           
-        $_SESSION['FULLNAME'] = $fbfullname;
-	    $_SESSION['EMAIL'] =  $femail;
+ 	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
+	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
+    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
+    $url = $_SESSION['url'];
+    checkuser($fbid,$fbfullname,$femail); // To update local DB
+
+    /* ---- Session Variables -----*/
+    $_SESSION['FBID'] = $fbid;           
+    $_SESSION['FULLNAME'] = $fbfullname;
+    $_SESSION['EMAIL'] =  $femail;
+
     /* ---- header location after session ----*/
-     
-    //$url1 = parse_url("http://e-haaletus.azurewebsites.net/$url", PHP_URL_PATH);
-    //$url2 = parse_url("http://e-haaletus.azurewebsites.net/logisisse.php", PHP_URL_PATH);
     $url2 = "/logisisse.php";
     if ($url == $url2){
         header("Location: http://e-haaletus.azurewebsites.net/kandideerimine.php");
     }else {
         header("Location: http://e-haaletus.azurewebsites.net/$url");
     }
-
 } else {
     $loginUrl = $helper->getLoginUrl();
     header("Location: ".$loginUrl);
--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php include "header.php"; ?>
-  <div class="container">
-    <div class="middle">    
-      <div class="row">
-        <div class="col-xs-8 col-md-4  col-md-offset-1">
-
-          <a href="http://e-haaletus.azurewebsites.net/kandidaadid.php" class="wide blue"><img src="picid/people-icon-oige.png" alt="kandidaadid" class="pilt">KANDIDAADID</a>
+    <div class="container">
+        <div class="middle">    
+            <div class="row">
+                <div class="col-xs-8 col-md-4  col-md-offset-1">
+                    <a href="http://e-haaletus.azurewebsites.net/kandidaadid.php" class="wide blue"><img src="picid/people-icon-oige.png" alt="kandidaadid" class="pilt">KANDIDAADID</a>
+                </div>
+                <div class="col-xs-8 col-md-4 col-md-offset-1">
+                    <a href="http://e-haaletus.azurewebsites.net/statistika.php" class="wide blue"><img src="picid/statistics.png" alt="statistika" class="pilt2">STATISTIKA</a>
+                </div>
+                <div class="col-xs-8 col-md-4 col-md-offset-1">
+                    <a href="http://e-haaletus.azurewebsites.net/tulemused.php" class="wide blue"><img src="picid/tulemused.png" alt="tulemused" class="pilt">TULEMUSED</a>
+                </div>
+                <?php if ($_SESSION['FBID']): ?>
+                <div class="col-xs-8 col-md-4 col-md-offset-1">
+                    <a href="http://e-haaletus.azurewebsites.net/kandideerimine.php" id="kandideerimine" class="wide blue"><img src="picid/people-icon-oige.png" alt="kandideeri" class="pilt">KANDIDEERI</a>
+                </div>
+                <?php endif ?>
+            </div>
         </div>
-        <div class="col-xs-8 col-md-4 col-md-offset-1">
-          <a href="http://e-haaletus.azurewebsites.net/statistika.php" class="wide blue"><img src="picid/statistics.png" alt="statistika" class="pilt2">STATISTIKA</a>
-        </div>
-        <div class="col-xs-8 col-md-4 col-md-offset-1">
-          <a href="http://e-haaletus.azurewebsites.net/tulemused.php" class="wide blue"><img src="picid/tulemused.png" alt="tulemused" class="pilt">TULEMUSED</a>
-        </div>
-          <?php if ($_SESSION['FBID']): ?>
-        <div class="col-xs-8 col-md-4 col-md-offset-1">
-          <a href="http://e-haaletus.azurewebsites.net/kandideerimine.php" id="kandideerimine" class="wide blue"><img src="picid/people-icon-oige.png" alt="kandideeri" class="pilt">KANDIDEERI</a>
-        </div>
-      <?php endif ?>
-      </div>
     </div>
-  </div>
 <?php include "footer.php"; ?>
