5b07f3e9d6246e124c3604b2a3eb761c1e460156
Hilari <hilarimuts@gmail.com>
2015-04-04 15:39:10 +0300

    katse, et kas lõhub ära midagi

--- header.php
+++ header.php
@@ -19,12 +19,6 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI'];
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
   <link rel="stylesheet" type="text/css" href="kujundus.css">
-
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"  type="text/javascript"></script>
-
-  <!-- Latest compiled JavaScript -->
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"  type="text/javascript"></script>
 </head>
 <body>
 <header>
