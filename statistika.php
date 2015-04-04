23925e71aa82fb51799e2dfc44583a1913ca7c9d
Hilari <hilarimuts@gmail.com>
2015-04-05 00:17:21 +0300

    vale rida -.-

--- statistika.php
+++ statistika.php
@@ -15,8 +15,9 @@ include "header.php";
 			<div id="container2" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto">
 			</div>
 		</div>	
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"  type="text/javascript"></script>
-</div><script src="statistika.js" type="text/javascript"></script>
+<script src="statistika.js" type="text/javascript"></script>
 <script src="highcharts.js" type="text/javascript"></script>
 <script src="exporting.js" type="text/javascript"></script>
 <?php include "footer.php"; ?>
