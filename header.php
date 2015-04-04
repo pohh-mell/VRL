54894331f9b10cc8a8174de12e605c6ea073dd83
Hilari <hilarimuts@gmail.com>
2015-04-04 21:54:41 +0300

    üks pisiasi ununes sry

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI'];
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link href='http://fonts.googleapis.com/css?family=Old+Standard+TT:700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="/css/kujundus.css">
+    <link rel="stylesheet" type="text/css" href="kujundus.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"  type="text/javascript"></script>
     <!-- Latest compiled JavaScript -->
