5dbd2ab70d9c6f33cd9ecc007b83e77ae2c52315
Hilari <hilarimuts@gmail.com>
2015-04-06 00:23:12 +0300

    üks kujunduse möll paigast

--- kandidaadid.php
+++ kandidaadid.php
@@ -6,32 +6,32 @@ include "header.php";
 	<div class="container">
 		<div class="middle">		
 			<ol class="singleline">				
-			<li><select class="Valikud1"  >
-				<option value="">------</option>
-	   		    <option value="1">Kogu Eesti</option>
-			    <option value="asd">asd</option>
-			    <option value="Võrumaa">Võrumaa</option>
-			  	<option value="Valgamaa">Valgamaa</option>
-			</select></li>					
-			<li><select class="Valikud">	
-				<option value="tühi">------</option>
-				<?php
-				require_once("andmed.php");
-				$conn=database();
-				//Query the database
-				$resultSet = $conn->query("SELECT nimi FROM erakonnad group by nimi;");
-				if($resultSet->num_rows != 0){
-					while($rows = $resultSet->fetch_assoc()){
-						$errakond = $rows['nimi'];
-						echo"<option value=\"$errakond\">$errakond</option>";
+				<li><select class="Valikud"  >
+					<option value="">------</option>
+		   		    <option value="1">Kogu Eesti</option>
+				    <option value="asd">asd</option>
+				    <option value="Võrumaa">Võrumaa</option>
+				  	<option value="Valgamaa">Valgamaa</option>
+				</select></li>					
+				<li><select class="Valikud">	
+					<option value="tühi">------</option>
+					<?php
+					require_once("andmed.php");
+					$conn=database();
+					//Query the database
+					$resultSet = $conn->query("SELECT nimi FROM erakonnad group by nimi;");
+					if($resultSet->num_rows != 0){
+						while($rows = $resultSet->fetch_assoc()){
+							$errakond = $rows['nimi'];
+							echo"<option value=\"$errakond\">$errakond</option>";
+						}
 					}
-				}
-				$conn->close();
-				?>
-			</select></li>
-			<li><form method="get" action="">
-				<input type="text" class="Valikud" name="search" id="search-text"  value="" placeholder="Sisesta kandidaadi nimi">
-			</form></li>
+					$conn->close();
+					?>
+				</select></li>
+				<li><form method="get">
+					<input type="text" class="Valikud" name="search" id="search-text"  value="" placeholder="Sisesta kandidaadi nimi">
+				</form></li>
 			</ol>			
 			<table id="t01"></table>
 		</div>
