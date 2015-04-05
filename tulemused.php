0b9833f7b8ad24e1e4f4dfc3ce7d646a08fc642a
pohh-mell <meelissulg1@gmail.com>
2015-04-05 22:28:39 +0300

    hilarionattentionwhore

--- tulemused.php
+++ tulemused.php
@@ -8,8 +8,8 @@ include "header.php";
 		
 			<ol class="singleline">			
 		
-			<a href="#C4">Erakonnad</a>
-			<a href="#C4">Kandidaadid</a>
+			<a href="#t01">Erakonnad</a>
+			<a href="#t02">Kandidaadid</a>
 			
 			</ol>
 			
@@ -48,6 +48,61 @@ include "header.php";
 					mysql_close($conn);
 					?>
 			</table>
+			<?php
+			<table id="t02">
+$host = "eu-cdbr-azure-north-c.cloudapp.net";
+	$user = "bb8f29df6ad035";
+	$pwd = "461b6fa7";
+	$db = "ehaaletusdata";
+$con = mysqli_connect($host, $user, $pwd, $db);
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}	
+
+
+	
+					$sql = "SELECT kandidaadid.id AS Number,kandidaadid.Nimi AS Nimi, kandidaadid.Piirkond AS Piirkond,
+					 erakonnad.Nimi AS Erakond, kandidaadid.haali AS Hääli
+						FROM kandidaadid  LEFT JOIN erakonnad 
+						ON kandidaadid.Erakonna_id=erakonnad.id
+						GROUP BY kandidaadid.id;";
+					$result = $con->query($sql);
+					echo "<tr>
+							<th>Nr</th>
+							<th>Nimi</th>
+							<th>Piirkond</th>
+							<th>Erakond</th>
+							<th>Hääli</th>
+							<th></th>
+							</tr>";
+					if($result->num_rows != 0){
+						while($rows = $result->fetch_assoc()){
+							$nr = $rows["Number"];
+							$Nimi = $rows["Nimi"];
+							$Piirkond = $rows["Piirkond"];
+							$Erakond = $rows["Erakond"];
+							$Hääli = $rows["Hääli"];
+
+							echo "<tr>
+							<td>$nr</td>
+							<td>$Nimi</td>
+							<td>$Piirkond</td>
+							<td>$Erakond</td>
+							<td>$Hääli</td>
+							<td>Hääleta</td>
+							</tr>";
+						}
+					}
+					else{
+    				echo "0 results";
+					}
+
+					mysql_close($con);
+
+
+				
+					?>
+				</table>
 		</div>
 	</div>
 <?php include "footer.php"; ?>
