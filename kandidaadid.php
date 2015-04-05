b692963438414dde17dd534ec3c7d601e214631d
KarlEST <karl.l22ts@hotmail.com>
2015-04-05 17:52:34 +0300

    } oli puudu. CORALLLLLL

--- kandidaadid.php
+++ kandidaadid.php
@@ -7,7 +7,7 @@ include "header.php";
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript">
 	window.addEventListener("load", function(){
-        getTable();
+        getTable();}
 	, true);
 
 
