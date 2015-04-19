62b7e2f105ff79925bdeb338f4ffd99a337e75fe
pohh-mell <meelissulg1@gmail.com>
2015-04-19 17:46:17 +0300

    prok

--- tulemused.php
+++ tulemused.php
@@ -14,87 +14,52 @@ include "header.php";
 			</ol>
 			
 			<table class="Era" id ="t01">
-				<?php
-					
-					require_once("andmed.php");
-					$conn=database();
-					$sql = "SELECT erakonnad.Nimi AS Erakond, erakonnad.Esimees AS Esimees, IFNULL(sum(kandidaadid.haali),0) AS Hääli
-							FROM erakonnad 
-							LEFT JOIN kandidaadid ON erakonnad.id=kandidaadid.Erakonna_id 
-							GROUP BY Erakond;";
-					$result = $conn->query($sql);
-					echo "<tr>
+				<tr>
 							<th>Erakond</th>
 							<th>Esimees</th>
 							<th>Hääli</th>
-							</tr>";
-					if($result->num_rows != 0){
-						while($rows = $result->fetch_assoc()){
-							$Erakond = $rows["Erakond"];
-							$Esimees = $rows["Esimees"];
-							$Hääli = $rows["Hääli"];
-
-							echo "<tr>
-							<td>$Erakond</td>
-							<td>$Esimees</td>
-							<td>$Hääli</td>
-							</tr>";
-						}
-					}
-					else{
-    				echo "0 results";
-					}
-
-					mysql_close($conn);
-					?>
+							</tr><tr>
+							<td>Java</td>
+							<td>Karl</td>
+							<td>103</td>
+							</tr><tr>
+							<td>Java Naiskond</td>
+							<td>Meelis</td>
+							<td>1</td>
+							</tr><tr>
+							<td>püüton</td>
+							<td>lärr</td>
+							<td>0</td>
+							</tr><tr>
+							<td>Uss</td>
+							<td>Keili</td>
+							<td>3</td>
+							</tr>
 			</table>
 			<table class ="kand" id="t01">
-			<?php
-			require_once("andmed.php");
-					$conn=database();
-
-	
-					$sql = "SELECT kandidaadid.id AS Number,kandidaadid.Nimi AS Nimi, kandidaadid.Piirkond AS Piirkond,
-					 erakonnad.Nimi AS Erakond, kandidaadid.haali AS Hääli
-						FROM kandidaadid  LEFT JOIN erakonnad 
-						ON kandidaadid.Erakonna_id=erakonnad.id
-						GROUP BY kandidaadid.id;";
-					$result = $con->query($sql);
-					echo "<tr>
+					<tr>
 							<th>Nr</th>
 							<th>Nimi</th>
 							<th>Piirkond</th>
 							<th>Erakond</th>
 							<th>Hääli</th>
-							<th></th>
-							</tr>";
-					if($result->num_rows != 0){
-						while($rows = $result->fetch_assoc()){
-							$nr = $rows["Number"];
-							$Nimi = $rows["Nimi"];
-							$Piirkond = $rows["Piirkond"];
-							$Erakond = $rows["Erakond"];
-							$Hääli = $rows["Hääli"];
-
-							echo "<tr>
-							<td>$nr</td>
-							<td>$Nimi</td>
-							<td>$Piirkond</td>
-							<td>$Erakond</td>
-							<td>$Hääli</td>
-							<td>Hääleta</td>
-							</tr>";
-						}
-					}
-					else{
-    				echo "0 results";
-					}
-
-					mysql_close($con);
-
-
-				
-					?>
+							</tr>
+							<tr>
+							<td>1</td>
+							<td>Karl</td>
+							<td>Tartu</td>
+							<td>Java</td>
+							<td>007</td>
+							</tr>
+							<tr>
+							<td>2</td>
+							<td>Hilx</td>
+							<td>Tartu</td>
+							<td>Java Naiskond</td>
+							<td>2</td>
+							</tr>
+							
+						
 				</table>
 		</div>
 	</div>
