be7755f29ac142c1d38970ccbcb376b94dab6fea
Hilari <hilarimuts@gmail.com>
2015-04-28 11:08:16 +0300

    123

--- statistika.php
+++ statistika.php
@@ -7,7 +7,7 @@ include "header.php";
 	<div class="container">
 		<div class="middle">
 			<ol class="singleline">
-			<li><select> <!-- onchange="funktioon();" -->
+			<li><select onchange="funktioon();"> <!-- onchange="funktioon();" -->
 	   		    <option value="Kogu Eesti">Kogu Eesti</option>
 			    <option value="Tartumaa">Partei</option>
 			</select></li>
@@ -18,6 +18,6 @@ include "header.php";
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"  type="text/javascript"></script>
 <script src="highcharts.js" type="text/javascript"></script>
-<script src="exporting.js" type="text/javascript"></script>
-<!--<script src="statistika.js" type="text/javascript"></script>-->
+<script src="exporting.js" type ="text/javascript"></script>
+<script src="statistika.js" type="text/javascript"></script>
 <?php include "footer.php"; ?>
