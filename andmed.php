baf57f6ba475c6767146727fc7a2e94b5a86cfc4
pohh-mell <meelissulg1@gmail.com>
2015-05-21 19:22:43 +0300

    o

--- andmed.php
+++ andmed.php
@@ -1,29 +1,39 @@
 <?php
 
-function database($valik){
-    $sql=$valik;
+
 	$host = "eu-cdbr-azure-north-c.cloudapp.net";
 	$user = "bb8f29df6ad035";
 	$pwd = "461b6fa7";
 	$db = "ehaaletusdata";
-    $valjastring="";
+    $valjalist= array();
+    $sql = "SELECT kandidaadid.id as Number,kandidaadid.nimi as Nimi,piirkond as Piirkond ,x.Nimi as Erakond, IFNULL(abi,0) as Hääli FROM kandidaadid
+left join (select Haal,count(Haal) as abi from users group by users.haal) as t on kandidaadid.id=t.Haal
+left join erakonnad as x on kandidaadid.Erakonna_id=x.id
+order by Number;";
+
+
 mysql_set_charset('utf8', $db);
 	// Create connection
+
+
 $conn = new mysqli($host, $user, $pwd, $db);
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
 	}
 $result=  $conn->query($sql);
+
+
 if($result->num_rows != 0){
     while($rows = $result->fetch_assoc()){
+            $abilist = array();
             $Nimi = $rows["Nimi"];
             $Hääli = $rows["Hääli"];
-            $abi = $Nimi . "," . $Hääli ';';
-            $valjastring .= $abi;
+            array_push($abilist, $Nimi, $Hääli);
+            array_push($valjalist, $abilist);
 
 
                         }
                     }
-    echo $valjastring;
-	return $valjastring;
+    print_r($valjalist);
 }
+?>
