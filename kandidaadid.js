18f7c2ad0b249f873717da8ff399e11424853ac1
Hilari <hilarimuts@gmail.com>
2015-05-25 00:44:09 +0300

    amlekfneiopg

--- kandidaadid.js
+++ kandidaadid.js
@@ -19,18 +19,3 @@ function getTable(){
     xmlhttp.send();
 }
 
-function getTable(){
-    if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-    } else { // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-          document.getElementById("users-grid").innerHTML=xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET","katse.php",true);
-    xmlhttp.send();
-}
